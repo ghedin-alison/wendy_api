e8348a28c379bbaf1dc548313c0c58b5334293e6
ghedin-alison <alison.ghedin@outlook.com>
2022-04-29 05:15:30 -0300

    adicionadas duas tabelas

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.Charfield(max_lenght=200)
+    image = models.Charfield(max_lenght=200)
+    likes = models.PositiveIntegerField(default=0)
+
+class User(models.Model):
+    pass

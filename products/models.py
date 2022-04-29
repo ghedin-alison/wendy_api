8e1f61ffd66fd33e876281c25f7de6a589244052
ghedin-alison <alison.ghedin@outlook.com>
2022-04-29 05:40:24 -0300

    adicionado configuracoes iniciais da views.py

--- products/models.py
+++ products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.Charfield(max_lenght=200)
-    image = models.Charfield(max_lenght=200)
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
     likes = models.PositiveIntegerField(default=0)
 
 class User(models.Model):
--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request): # /api/products (get)
+        pass
+
+
+    def create(self, request): # /api/products (post)
+        pass
+
+
+    def retrieve(self, request, pk=None): # /api/products/<str:id>
+        pass
+
+    def update(self, request, pk=None): # /api/products/<str:id>
+        pass
+
+    def kill(self, request, pk=None): # /api/products/<str:id>
+        pass
